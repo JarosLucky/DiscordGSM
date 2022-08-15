1abf6705aee3dfc1ea5bc023d410487788f64195
JarosLucky <19414192+JarosLucky@users.noreply.github.com>
2022-08-15 23:39:41 +0700

    fix decoding

--- getbotinvitelink.py
+++ getbotinvitelink.py
@@ -7,5 +7,5 @@ TOKEN = os.getenv('DGSM_TOKEN')
 segs = TOKEN.split('.')
 assert len(segs) == 3, "invalid token"
 
-clientid = base64.b64decode(segs[0]).decode()
+clientid = base64.b64decode(segs[0] + "==").decode()
 print(f"https://discord.com/api/oauth2/authorize?client_id={clientid}&permissions=339008&scope=bot")
